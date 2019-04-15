908a64d3aab764befc90d0d7dc78266188638921
chengong1 <chengong1@xiaomi.com>
2019-04-15 11:14:00 +0800

    Initial Commit

---
+++ DouFa/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DouFa
+//
+//  Created by KyleChen on 2019/4/15.
+//  Copyright © 2019 KCG. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ DouFa/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DouFa
+//
+//  Created by KyleChen on 2019/4/15.
+//  Copyright © 2019 KCG. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
